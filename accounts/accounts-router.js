530fc5537343643738697b5d3ae8a15cced46968
sweyome <sweyome1@gmail.com>
2019-08-21 20:56:47 -0400

    Post/insert end-point done

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -14,4 +14,40 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  db('accounts')
+    .where({ id: req.params.id })
+    .first()
+    .then(account => {
+      if (account) {
+        res.status(200).json(account);
+      } else {
+        res.status(404).json({ message: 'Account not found' });
+      }
+    });
+});
+
+router.post('/', (req, res) => {
+  if (accountIsValid(req.body)) {
+    db('accounts')
+      .insert(req.body, 'id')
+      .then(([id]) => id)
+      .then(id => {
+        db('accounts')
+          .where({ id })
+          .first()
+          .then(account => {
+            res.status(201).json(account);
+          });
+      })
+      .catch(() => {
+        res.status(500).json({ message: 'Could not add the account' });
+      });
+  } else {
+    res.status(400).json({
+      message: 'Please provide name and budget of zero or more for the account',
+    });
+  }
+});
+
 module.exports = router;
